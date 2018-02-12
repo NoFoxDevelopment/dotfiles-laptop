c142211708ec3047cc789c792b15822f8ca1340d
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2018-02-12 09:17:36 -0500

    Added .vimrc & minor updates

--- .config/i3/scripts/net.sh
+++ .config/i3/scripts/net.sh
@@ -1,39 +1,58 @@
-#!/bin/bash 
+#!/bin/sh
+# Copyright (C) 2014 Julien Bonjean <julien@bonjean.info>
 
-# Show Wifi Stuff
-W_inter=$(ip link | grep "[1-9]: wlp" | cut -d " " -f2 | tr -d ':')
-W_con=$(nmcli d | grep "$W_inter" | awk '{print $3}')
-W_name=$(nmcli d | grep "$W_inter" | awk '{print $4}')
-W_ip=$(ifconfig $W_inter | grep 'netmask' | awk '{print $6}')
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-# Show Ethernet stuff
-E_inter=$(ip link | grep "^[1-9]: enp" | cut -d " " -f2 | tr -d :)
-E_con=$(nmcli d | grep "$E_inter" | awk '{print $3}')
-E_name=$(nmcli d | grep "$E_inter" | awk '{print $4}')
-E_ip=$(ifconfig $E_inter | grep 'netmask' | awk '{print $6}')
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Wifi () {
-if [ "$W_con" = "connected" ];then 
-    echo -e " $W_ip ($W_name)"
-fi 
+INTERFACE="${BLOCK_INSTANCE:-eth0}"
+
+state () {
+  cat /sys/class/net/$INTERFACE/operstate
 }
 
-Ethernet () {
-if [ "$E_con" = "connected" ];then 
-    echo -e " $E_ip ($E_name)"
-fi 
+speed () {
+  cat /sys/class/net/$INTERFACE/speed 2> /dev/null
 }
 
-case "$1" in 
-    "-w" )
-        Wifi
-    ;;
-    "-e" )
-        Ethernet
-    ;;
-    * )
-		echo -e " $0 : no Argument \n\t -e : Show Ethernet \n\t -w : Show Wireless"
-    ;;
-esac 
+ipv4 () {
+  ip addr show $INTERFACE | perl -n -e '/inet ([^\/]+)/ && print $1'
+}
 
+ipv6 () {
+  ip -6 addr show $INTERFACE | perl -n -e '/inet6 ([^\/]+)/ && print $1'
+}
+
+if [ "$(state)" != 'up' ]; then
+  echo down # full text
+  echo down # short text
+  echo \#FF0000 # color
+  exit 0
+fi
+
+# TODO eventually accept a -4 or -6 flag to force only one or the other
+IPADDR=$(ipv6)
+[ -z "$IPADDR" ] && IPADDR=$(ipv4)
+
+if [ -z "$IPADDR" ]; then
+  echo no addr # full text
+  echo no addr # short text
+  exit 0
+fi
+
+# full text
+echo -n "$IPADDR"
+SPEED=$(speed)
+[ -n "$SPEED" ] && echo " ($SPEED Mbits/s)" || echo
+
+# short text
+echo "$IPADDR"
--- .zshrc
+++ .zshrc
@@ -1,45 +1,13 @@
-
-# User Configuration
-POWERLEVEL9K_MODE='nerdfont-complete'
-POWERLEVEL9K_STATUS_CROSS=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator os_icon dir_writable dir rbenv vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time background_jobs history time)
-POWERLEVEL9K_LINUX_ICON=$' \uF1D1'
-POWERLEVEL9K_DIR_DEFAULT_BACKGROUND='111'
-POWERLEVEL9K_DIR_DEFAULT_FOREGROUND='black'
-POWERLEVEL9K_DIR_HOME_BACKGROUND='111'
-POWERLEVEL9K_DIR_HOME_FOREGROUND='black'
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND='111'
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND='black'
-POWERLEVEL9K_OK_ICON=
-POWERLEVEL9K_FAIL_ICON=
-POWERLEVEL9K_HOME_ICON=
-POWERLEVEL9K_HOME_SUB_ICON=
-POWERLEVEL9K_ROOT_INDICATOR_BACKGROUND='196'
-POWERLEVEL9K_ROOT_INDICATOR_FOREGROUND='yellow'
-# POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND='black'
-# POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND='white'
-POWERLEVEL9K_OS_ICON_BACKGROUND='black'
-POWERLEVEL9K_OS_ICON_FOREGROUND='white'
-POWERLEVEL9K_ROOT_ICON=$'\uE74F'
-POWERLEVEL9K_USER_ICON=
-POWERLEVEL9K_FOLDER_ICON=
-POWERLEVEL9K_LOCK_ICON=
-POWERLEVEL9K_EXECUTION_TIME_ICON=
-POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=$'\uE0C6'
-POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=$'\uE0C7'
-
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=~/.oh-my-zsh
+  export ZSH=/home/nofox/.oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="bira"
 
 # Set list of themes to load
 # Setting this variable when ZSH_THEME=random
@@ -101,7 +69,7 @@ source $ZSH/oh-my-zsh.sh
 # export MANPATH="/usr/local/man:$MANPATH"
 
 # You may need to manually set your language environment
-# export LANG=de_DE.UTF-8
+# export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
